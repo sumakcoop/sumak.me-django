8a06b2eeb0baca67c54a4c65e300538cedee1e6b
Joaquín Cuéllar <joa.cuellar@riseup.net>
2021-03-27 12:41:07 +0100

    real initial commit

---
+++ sumakMe/sumakMe/urls.py
@@ -0,0 +1,41 @@
+"""
+ This file is part of Sumak.me-django
+ Copyright (C) 2021 Asociación SUMAK <info (at) sumakcoop (dot) org
+ 
+ Sumak.me-django is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+ 
+ Sumak.me-django is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+ 
+ You should have received a copy of the GNU Affero General Public License
+ along with Sumak.me-django.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""sumakMe URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.apps import apps
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(apps.get_app_config('oscar').urls[0])),
+]
---
+++ sumakMe/sumakMe/wsgi.py
@@ -0,0 +1,34 @@
+"""
+ This file is part of Sumak.me-django
+ Copyright (C) 2021 Asociación SUMAK <info (at) sumakcoop (dot) org
+ 
+ Sumak.me-django is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+ 
+ Sumak.me-django is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+ 
+ You should have received a copy of the GNU Affero General Public License
+ along with Sumak.me-django.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""
+WSGI config for sumakMe project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sumakMe.settings')
+
+application = get_wsgi_application()
