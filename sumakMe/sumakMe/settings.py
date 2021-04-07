7259e37df50d5e9525dea18b2d8d715edc500d19
Joaquín Cuéllar <joa.cuellar@riseup.net>
2021-04-07 11:25:10 +0200

    oscar imports in settings came back

--- sumakMe/sumakMe/settings.py
+++ sumakMe/sumakMe/settings.py
@@ -28,7 +28,7 @@
 
 import os
 # oscar stuff import
-# from oscar.defaults import *
+from oscar.defaults import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
