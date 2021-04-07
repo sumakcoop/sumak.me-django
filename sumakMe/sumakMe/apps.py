b4fdfb685d5330ac3c1abac935300ad64565b105
Joaquín Cuéllar <joa.cuellar@riseup.net>
2021-04-07 11:22:27 +0200

    PEP8 compliance

--- sumakMe/sumakMe/apps.py
+++ sumakMe/sumakMe/apps.py
@@ -1,24 +1,24 @@
-"""
- This file is part of Sumak.me-django
- Copyright (C) 2021 Asociación SUMAK <info (at) sumakcoop (dot) org
+#
+# This file is part of Sumak.me-django
+# Copyright (C) 2021 Asociación SUMAK <info (at) sumakcoop (dot) org
+#
+# Sumak.me-django is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Sumak.me-django is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Sumak.me-django.  If not, see <http://www.gnu.org/licenses/>.
+#
 
- Sumak.me-django is free software: you can redistribute it and/or modify
- it under the terms of the GNU Affero General Public License as
- published by the Free Software Foundation, either version 3 of the
- License, or (at your option) any later version.
-
- Sumak.me-django is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Affero General Public License for more details.
-
- You should have received a copy of the GNU Affero General Public License
- along with Sumak.me-django.  If not, see <http://www.gnu.org/licenses/>.
-"""
-
-from django.conf.urls import url, include
 from oscar import config
 import re
+
 
 # class for overriding default oscar urls
 class MyShop(config.Shop):
--- sumakMe/sumakMe/wsgi.py
+++ sumakMe/sumakMe/wsgi.py
@@ -1,29 +1,30 @@
-"""
- This file is part of Sumak.me-django
- Copyright (C) 2021 Asociación SUMAK <info (at) sumakcoop (dot) org
- 
- Sumak.me-django is free software: you can redistribute it and/or modify
- it under the terms of the GNU Affero General Public License as
- published by the Free Software Foundation, either version 3 of the
- License, or (at your option) any later version.
- 
- Sumak.me-django is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Affero General Public License for more details.
- 
- You should have received a copy of the GNU Affero General Public License
- along with Sumak.me-django.  If not, see <http://www.gnu.org/licenses/>.
-"""
+#
+# This file is part of Sumak.me-django
+# Copyright (C) 2021 Asociación SUMAK <info (at) sumakcoop (dot) org
+#
+# Sumak.me-django is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# Sumak.me-django is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with Sumak.me-django.  If not, see <http://www.gnu.org/licenses/>.
+#
 
-"""
-WSGI config for sumakMe project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
-"""
+#
+# WSGI config for sumakMe project.
+#
+# It exposes the WSGI callable as
+# a module-level variable named ``application``.
+#
+# For more information on this file, see
+# https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+#
 
 import os
 
