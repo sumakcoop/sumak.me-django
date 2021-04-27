60e37084c23510868ab7cbc33159035f37dcfc56
Joaquín Cuéllar <joa.cuellar@riseup.net>
2021-04-27 19:02:39 +0200

    added an example index page based in official

--- sumakMe/sumakMe/settings.py
+++ sumakMe/sumakMe/settings.py
@@ -217,5 +217,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # oscar settings
 OSCAR_SHOP_NAME = 'SUMAK'
-OSCAR_SHOP_TAGLINE = 'Súmate: revierte tu cesta de la compra en proyectos sociales y medioambientales'
+OSCAR_SHOP_TAGLINE = 'Red de consumo de proximidad, ético, ecológico y sostenible con responsabilidad social y medioambiental'
 OSCAR_HOMEPAGE = 'index.html'
